fbce7e2b372fd56a5ce318c0a8404dde7c050747
MikeTeddyOmondi <mike_omondi@outlook.com>
2025-05-30 16:11:40 +0300

    added etcd, registry, ollama, stirling-pdf, tinyauth

---
+++ etcd/launch-etcd.sh
@@ -0,0 +1,9 @@
+# Launch Etcd
+docker run -d --name etcd-server \
+    --network proxy-net \
+    --publish 2379:2379 \
+    --publish 2380:2380 \
+    --env ALLOW_NONE_AUTHENTICATION=yes \
+    --env ETCD_ADVERTISE_CLIENT_URLS=http://etcd-server:2379 \
+    bitnami/etcd:3.6
+
