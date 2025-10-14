a2da5e6873862377f01a356566e37052058304f8
MikeTeddyOmondi <mike_omondi@outlook.com>
2025-10-14 16:42:00 +0300

    update dagger

--- dagger/start-dagger-engine.sh
+++ dagger/start-dagger-engine.sh
@@ -1,6 +1,6 @@
 docker run -d \
   -v /var/lib/dagger \
   \
-  --name dagger-engine-v0.14.0 \
+  --name dagger-engine-v0.18.19 \
   --privileged \
-  registry.dagger.io/engine:v0.14.0 # -v $HOME/.config/dagger/engine.json:/etc/dagger/engine.json \
+  registry.dagger.io/engine:v0.18.19 # -v $HOME/.config/dagger/engine.json:/etc/dagger/engine.json \
